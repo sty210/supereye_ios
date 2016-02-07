92de35166fe242aa21b1e3fdc9bfc89cc09534ca
송태양 <sty210@naver.com>
2016-02-07 17:42:37 +0900

    Initial Commit

---
+++ supereye_iosTests/supereye_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  supereye_iosTests.swift
+//  supereye_iosTests
+//
+//  Created by 송태양 on 2016. 2. 7..
+//  Copyright © 2016년 송태양. All rights reserved.
+//
+
+import XCTest
+@testable import supereye_ios
+
+class supereye_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
